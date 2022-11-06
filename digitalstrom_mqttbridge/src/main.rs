0a3500183216000eb70274db77d7c3815a572f44
Sönke K. Schau <github@schau.org>
2022-11-06 14:38:48 +0100

    changed logging functions

--- digitalstrom_mqttbridge/src/main.rs
+++ digitalstrom_mqttbridge/src/main.rs
@@ -4,11 +4,13 @@ extern crate dss_interface;
 extern crate dss_openapi;
 extern crate reqwest;
 
+extern crate phf;
 extern crate slog;
 extern crate slog_async;
 extern crate slog_term;
 
-use slog::{error, info, o, Drain};
+use phf::phf_map;
+use slog::{b, error, info, o, Drain};
 
 use crate::commandline::{Cli, Messageformat};
 use clap::Parser;
@@ -17,11 +19,53 @@ struct Context {
     log: slog::Logger,
     configuration: dss_openapi::apis::configuration::Configuration,
 }
+struct CombinedBorrowedKV<'a> {
+    k1: &'a slog::BorrowedKV<'a>,
+    k2: &'a slog::BorrowedKV<'a>,
+}
+impl slog::KV for CombinedBorrowedKV<'_> {
+    fn serialize(
+        &self,
+        record: &slog::Record,
+        serializer: &mut dyn slog::Serializer,
+    ) -> slog::Result {
+        self.k1.serialize(record, serializer)?;
+        self.k2.serialize(record, serializer)?;
+        Ok(())
+    }
+}
 
-//enum Messages {
-//    E0001,
+impl Context {
+    fn logevent(&self, messageid: i32, kv: slog::BorrowedKV) {
+        let z = match messageid / 1000 {
+            1 => (slog::Level::Error, 'E'),
+            2 => (slog::Level::Warning, 'I'),
+            3 => (slog::Level::Info, 'I'),
+            4 => (slog::Level::Debug, 'N'),
+            _ => (slog::Level::Critical, 'C'),
+        };
+        let recs = slog::record_static!(z.0, "");
+        self.log.log(&slog::Record::new(
+            &recs,
+            &format_args!("{}{}: {}", z.1, messageid, ERRORMESSAGES[&messageid]),
+            b!(CombinedBorrowedKV {
+                k1: &kv,
+                k2: &b!("messageid"=>messageid),
+            }),
+        ));
+    }
+}
+//enum MyEnum {
+//    /// Startupmessage
+//    I3000 = 3000,
+//    B = 456,
 //}
-
+static ERRORMESSAGES: phf::Map<i32, &'static str> = phf_map! {
+    3000i32 => concat!(env!("CARGO_PKG_NAME")," ", env!("CARGO_PKG_VERSION"), " - startup"),
+    2i32 => "apple",
+    3i32 => "apple",
+    4i32 => "request succeeded, but outcome was not ok",
+};
 #[tokio::main]
 async fn main() {
     let cargo_pkg_name = env!("CARGO_PKG_NAME");
@@ -70,23 +114,20 @@ async fn main() {
         },
     };
 
-    info!(
-        context.log,
-        "{} - {version_major}.{version_minor}.{version_patch}{version_pre} {event}",
-        cargo_pkg_name,
-        event="start",
-        version_major=cargo_pkg_version_major,
-        version_minor=cargo_pkg_version_minor,
-        version_patch=cargo_pkg_version_patch,
-        version_pre=cargo_pkg_version_pre,
-        ;
-        "eventid" => 1
+    context.logevent(
+        3000,
+        b!(
+            "version_major"=>cargo_pkg_version_major,
+            "version_minor"=>cargo_pkg_version_minor,
+            "version_patch"=>cargo_pkg_version_patch,
+            "version_pre"=>cargo_pkg_version_pre,
+        ),
     );
 
     match &cli.command {
         crate::commandline::Commands::Run {
-            mqttserver,
-            application_token,
+            mqttserver: _,
+            application_token: _,
         } => {}
         crate::commandline::Commands::RequestApplicationToken { application_name } => {
             let result = dss_openapi::apis::authentication_api::request_application_token(
@@ -97,13 +138,19 @@ async fn main() {
             match result {
                 Ok(response) => {
                     if !response.ok {
-                        error!(
-                            context.log,
-                            "outcome was not ok";
-                            "api" => "request_application_token",
-                            "message" => response.message.unwrap_or(String::from("<unknown>")),
-                            "eventid" => 4
-                        );
+                        context.logevent(
+                            4,
+                            b!(
+                                "api" => "request_application_token",
+                                "message" => response.message.unwrap_or(String::from("<unknown>")
+                            )
+                        ));
+                        //let recs = slog::record_static!(ERRORMESSAGES[&4].0, "");
+                        //context.log.log(&slog::Record::new(
+                        //    &recs,
+                        //    &format_args!("{}", ERRORMESSAGES[&4].1),
+                        //    b!("api" => "request_application_token","message" => response.message.unwrap_or(String::from("<unknown>")),"eventid"=>4),
+                        //));
                     } else {
                         let application_token = response.result.unwrap().application_token;
                         info!(
